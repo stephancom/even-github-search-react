0f14519745dae2a9bcfd833a80f92ed6b42c4407
stephan.com <stephan@stephan.com>
2018-12-30 18:05:20 -0500

    add React Router
    but don’t use it yet - this is just so we get the config when we eject…

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { Button } from 'reactstrap';
 import logo from './logo.svg';
 import './App.css';
