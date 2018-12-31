6483d4788e3d681eb9391c573d588b38dd1f4fe8
stephan.com <stephan@stephan.com>
2018-12-31 03:05:58 -0500

    GitHub Search working

--- src/containers/Search.js
+++ src/containers/Search.js
@@ -3,8 +3,10 @@ import { Container, Row, Col, Form, FormGroup, Label, Input, Button } from 'reac
 import axios from '../github_axios';
 
 const licenses = ["", "MIT", "ISC", "Apache", "GPL"];
+const searchPath = "/search/repositories";
+const starsRegexp = /^\s*(<|>|>=|<=)?(\*?|\d*)(\.{2,3}(\*?|\d+))?\s*$/;
 
-class Search extends Component {
+export default class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -16,9 +18,58 @@ class Search extends Component {
       starsValid: true,
       searching: false,
       error_response: null,
+      repositories: null,
+      searchOk: false
     }
 
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  paramOK(key) {
+    return this.state[key] && /\S/.test(this.state[key]);
+  }
+  paramEncode(key) {
+    return encodeURIComponent(this.state[key].trim());
+  }
+  isValid() {
+    console.log('isValid', this);
+    this.setState({
+      starsValid: this.state.stars === null || this.state.stars.length < 1 || starsRegexp.test(this.state.stars),
+      // topic can be blank if license or stars are filled in
+      topicValid: this.state.stars != null || this.state.license != null || (this.state.topic != null && this.state.topic.length > 0)
+    });
+
+    return this.state.topicValid && this.state.starsValid;
+  }
+  clearValid() { 
+    this.setState({starsValid: true, topicValid: true });
+  }
+  urlParams() {
+    var params = {};
+    if (this.paramOK("topic"))   { params["topic"] = this.state.topic; }
+    if (this.paramOK("stars"))   { params["stars"] = this.state.stars; }
+    if (this.paramOK("license")) { params["license"] = this.state.license; }
+    if (this.paramOK("fork"))    { params["fork"] = true; }
+    return params;
+  }
+  queryParams() {
+    var query = [];
+    if (this.paramOK("topic"))   { query.push(this.paramEncode("topic")); }
+    if (this.paramOK("stars"))   { query.push("stars:" + this.paramEncode("stars")); }
+    if (this.paramOK("license")) { query.push("license:" + this.paramEncode("license")); }
+    if (this.paramOK("fork"))    { query.push("fork:true"); }
+    return query;
+  }
+
+  queryMessage() {
+    if(this.state.repositories === null) {
+      return "Please enter query and click SEARCH above, results appear here";
+    } else if(this.state.repositories.length === 0) {
+      return "No results";
+    } else {
+      return "SEARCH results"
+    }
   }
 
   handleInputChange(event) {
@@ -26,15 +77,29 @@ class Search extends Component {
     const value = target.type === 'checkbox' ? target.checked : target.value;
     const name = target.name;
 
-    this.setState({
-      [name]: value
-    });
+    this.setState({ [name]: value });
   }
 
   handleSubmit(event) {
-    this.setState({searching: true});
-    // TODO
-    this.setState({searching: false});
+    event.preventDefault();
+    if (!this.isValid()) { return; }
+    var q = this.queryParams();
+    this.setState({repositories: [], searching: true, error_response: null});
+
+    // RIGHT (but GitHub expects something weird)
+    // axios.get(searchPath, { params: { q: q.join('+') } })
+    // WRONG (but it works)
+    axios
+      .get(searchPath + "?q=" + q.join("+"))
+      .then(response => {
+        this.setState({repositories: response.data.items});
+      })
+      .catch(error => {
+        this.setState({repositories: null, error_response: error["response"]["data"]});
+      })
+      .then(() => {
+        this.setState({searching: false});
+      });
   }
 
   render() {
@@ -90,24 +155,32 @@ class Search extends Component {
         <hr/>
         <Row>
           <Col className="text-center m-5">
-            <img alt="searching..." v-if="this.state.searching" src="SpinnyBalls.gif" />
-            <p v-else="v-else">
-              <errors v-if="error_response" data={this.error_response}></errors>
-              <span v-else-if="repositories == null">Please enter query and click SEARCH above, results appear here</span>
-              <span v-else-if="repositories.length == 0">No results</span><span v-else="v-else">SEARCH results</span>
-            </p>
+            { this.state.searching ? (
+              <img alt="searching..." v-if="this.state.searching" src="SpinnyBalls.gif" />
+            ) : (
+              <p>
+                { this.state.error_response ? (
+                  <errors data={this.error_response}></errors>
+                ) : (
+                  <span>{this.queryMessage()}</span>
+                ) }
+              </p>
+            ) }
           </Col>
         </Row>
-        <Row>
-          <Col>
-            <repository v-for="repo in repositories" repo="repo"></repository>
-          </Col>
-        </Row>
+        { this.state.repositories != null &&
+          <Row>
+            <Col>
+              <ol>
+                {
+                  this.state.repositories.map((repo, i) => <li key={i}>{repo.name}</li>)
+                }
+              </ol>
+              <repository v-for="repo in repositories" repo="repo"></repository>
+            </Col>
+          </Row>
+        }
       </Container>
     );
   }
 }
-
-export default Search;
-
-
